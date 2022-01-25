de5f77ee051ab277f24c7775740e76c4bce234cd
Bones <ahansen1335@gmail.com>
2022-01-25 17:57:05 +0100

    create Player Factory Function and update gameBoard module

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 // gameBoard Module
 const gameBoard = (() => {
-    let board = ['O', 'X', 'X',
+    let _board = ['O', 'X', 'X',
                  'X', 'X', 'O',
                  'O', 'O', 'X'];
+    const getBoard = () => _board;
+    const clearBoard = _board => {
+        _board = ['', '', '',
+                  '', '', '',
+                  '', '', ''];
+    };
     return {
-        board
+        getBoard,
+        clearBoard
     };
 })();
 
 // Player Factory Function
 
+const Player = (name) => {
+    const getName = () => name;
+    const iconSelection = icon => {
+        let player1 = '';
+        let player2 = '';        
+        if (icon === 'X') {
+            player1 = 'X';
+            player2 = 'O';
+        } else if (icon === 'O') {
+            player1 = 'O';
+            player2 = 'X';
+        }
+    };
+    
+    return {getName, iconSelection}
+};
